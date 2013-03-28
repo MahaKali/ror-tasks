25936f76705cd379a8bc5445a29ca40e1dcf8c1c
Joanna Banas <09_banas@wierzba.wzks.uj.edu.pl>
2013-03-28 04:53:54 +0100

    task-2

--- task-2/lib/todo_list.rb
+++ task-2/lib/todo_list.rb
@@ -1,6 +1,71 @@
 class TodoList
 
   # Initialize the TodoList with +items+ (empty by default).
-  def initialize(items=[])
+  def initialize(items=nil)
+    if items[:db] == nil
+      raise IllegalArgument
+    else
+      @database = items[:db]
+			@network = items[:social_network]
+		end
   end
+
+  def empty?()
+    @database.items_count == 0
+  end
+ 
+  def size()
+    @database.items_count
+  end 
+
+  def <<(other_object)
+    if other_object == nil or other_object[:title] == "" or	other_object[:title].size < 3
+			nil
+		else
+			# other_object[:title] = other_object[:title][0...255]
+      cut_title(other_object)
+			
+			@database.add_todo_item(other_object)
+			if @network
+				if other_object[:description] == ""
+					@network.spam("[" + other_object[:title] + "] missing description")
+				else
+					@network.spam("[" + other_object[:title] + "] I am going to " + other_object[:description])
+				end
+			end
+		end
+  end
+
+	def first()
+		if @database == nil
+			nil
+		else
+    	@database.get_todo_item(0)
+		end
+  end
+
+  def last()
+    if self.size > 0
+      @database.get_todo_item(self.size - 1)
+    else
+      nil
+    end
+  end
+
+  def toggle_state(index)
+		item = @database.get_todo_item(index)
+		if item == nil
+			raise IllegalArgument
+		else
+			# item[:title] = item[:title][0...255]
+			cut_title(item)
+			
+			@database.todo_item_completed?(index) ? @database.complete_todo_item(index, false) : @database.complete_todo_item(index, true)
+   		@network.spam("[" + item[:title] + "] " + item[:description] + " is done") if @network
+	  end
+	end
+	
+	def cut_title(item)
+		item[:title] = item[:title][0...255]
+	end
 end
--- task-2/spec/todo_list.rb
+++ task-2/spec/todo_list.rb
@@ -5,7 +5,7 @@ require_relative '../lib/exceptions'
 describe TodoList do
   subject(:list)            { TodoList.new(db: database) }
   let(:database)            { stub }
-  let(:item)                { Struct.new(:title,:description).new(title,description) }
+	let(:item)                { Struct.new(:title,:description).new(title,description) }
   let(:title)               { "Shopping" }
   let(:description)         { "Go to the shop and buy toilet paper and toothbrush" }
 
@@ -38,13 +38,17 @@ describe TodoList do
   end
 
   it "should persist the state of the item" do
-    mock(database).todo_item_completed?(0) { false }
+    mock(database).get_todo_item(0) { item }
+		
+		mock(database).todo_item_completed?(0) { false }
     mock(database).complete_todo_item(0,true) { true }
     mock(database).todo_item_completed?(0) { true }
     mock(database).complete_todo_item(0,false) { true }
 
     list.toggle_state(0)
-    list.toggle_state(0)
+    
+		mock(database).get_todo_item(0) { item }
+		list.toggle_state(0)
   end
 
   it "should fetch the first item from the DB" do
@@ -65,7 +69,15 @@ describe TodoList do
     list.last.should == nil
   end
 
-  context "with empty title of the item" do
+  it "should return nil for the first and the last item if the db is empty" do
+    stub(database).items_count { 0 }
+
+    mock(database).get_todo_item(0) { nil } 
+    list.first.should == nil
+    list.last.should == nil
+  end
+
+	context "with empty title of the item" do
     let(:title)   { "" }
 
     it "should not add the item to the DB" do
@@ -74,4 +86,102 @@ describe TodoList do
       list << item
     end
   end
+	
+	context "with nil item" do
+		let(:item) { nil }
+		
+
+		it "should raise an exception when changing the item state if the item is nil" do
+			mock(database).get_todo_item(0) { item }
+			
+			expect{ list.toggle_state(0)}.to raise_error(IllegalArgument)
+		end
+
+		it "should not accept a nil item" do
+			dont_allow(database).add_todo_item(item)
+			
+			list << item
+		end
+	end
+
+	context "with too short title" do
+		let(:title) { "A"}
+
+		it "should not accept an item with too short (but not empty) title" do
+			dont_allow(database).add_todo_item(item)
+
+			list << item
+		end
+	end
+	
+  context "with missing description" do
+		let(:description) {}
+			
+		it "should accept an item with missing description" do
+			mock(database).add_todo_item(item) { true } 
+			list << item
+		end
+	end
+
+	context "with social network" do
+		subject(:list)					{ TodoList.new(db: database, social_network: network) }
+		let(:network)						{ stub! }
+		let(:add_prefix)				{ "I am going to " }
+		let(:complete_suffix)		{ " is done" }
+
+	  it "should notify social network if an item is added to the list" do
+			mock(database).add_todo_item(item) { true }
+			mock(network).spam("[" + title + "] " + add_prefix + description) { true }
+			list << item
+		end
+
+		it "should notify social network if an item is completed" do
+			mock(network).spam("[" + title + "] " + description + complete_suffix) { true }
+			
+			mock(database).get_todo_item(0) { item }
+			mock(database).todo_item_completed?(0) { false }
+			mock(database).complete_todo_item(0, true)
+			
+			list.toggle_state(0)
+		end
+		
+		context "with missing title" do
+			let(:title) { "" } 
+
+			it "should not notify social network if the title od the item is missing" do
+				dont_allow(network).spam
+				list << item
+			end
+		end
+
+		context "with missing description" do
+			let(:description) { "" }
+
+			it "should notify social network if the body of the item is missing" do
+				mock(database).add_todo_item(item) { true }
+				mock(network).spam("[" + title + "] missing description") { true }
+				list << item
+			end
+		end
+
+		context "with too long title" do
+			let(:title) { "q"*256 }
+
+			it "should cut the title of the item when notifying social network about added item if it is longer than 255 chars" do
+				mock(database).add_todo_item(item) { true }
+				mock(network).spam("[" + title[0...255] + "] " + add_prefix + description) { true }
+				list << item
+			end
+
+			it "should cut thetitle of the item when notifying social network about completed item if it is longer than 255 chars" do
+				mock(network).spam("[" + title[0...255] + "] " + description + complete_suffix) { true }
+
+				mock(database).get_todo_item(0) { item }
+				mock(database).todo_item_completed?(0) { false }
+				mock(database).complete_todo_item(0, true)
+
+				list.toggle_state(0)
+			end
+		end
+	end
 end
